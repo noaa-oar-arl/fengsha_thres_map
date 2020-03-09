50606057f720b47f2a8fa1dbf9d2cacccd01a06e
Barry Baker <bbakernoaa@users.noreply.github.com>
2020-03-09 11:08:06 -0400

    add threshold for central plains of the US

--- make_fv3_thres.sh
+++ make_fv3_thres.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 #------------------------Set these here -------------
-#res='C384'
-res='C96'
-base_dir=emi_C96
-#base_dir=/gpfs/hps3/emc/naqfc/noscrub/Barry.Baker/emissions/emi_LIPAN_C384
+res='C384'
+#res='C96'
+#base_dir=emi_C96
+base_dir=/gpfs/hps3/emc/naqfc/noscrub/Barry.Baker/emissions/emi_LIPAN_C384
 
 #----------------------------------------------------
 for month in 01 02 03 04 05 06 07 08 09 10 11 12; do
@@ -28,4 +28,17 @@ for month in 01 02 03 04 05 06 07 08 09 10 11 12; do
     
     ./modify_latlon_box.py -ut $thres -d $directory -b $box -r $res
     
+    # MODIFY CENTRAL US
+    
+    thres="0.065,0.18,0.27,0.50,0.45,0.38,0.40,0.50,0.50,0.27,0.50,0.45,9999.0"
+    box="-126,25,-65,50"
+    
+    ./modify_latlon_box.py -ut $thres -d $directory -b $box -r $res
+    
+    # Modify Nothern Mexico for continuity
+    thres="0.065,0.18,0.27,0.30,0.35,0.38,0.45,0.41,0.41,0.27,0.50,0.45,9999.0" 
+    box="-117.95,18,-94.6,36"
+    
+    ./modify_latlon_box.py -ut $thres -d $directory -b $box -r $res
+        
 done
