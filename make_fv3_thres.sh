fa53bac8baa1fb352fc49e87208285f10180dd6a
Barry.Baker@noaa.gov <Barry.Baker@v72a2.ncep.noaa.gov>
2020-04-01 15:45:34 +0000

    make_fv3_thres.sh

--- make_fv3_thres.sh
+++ make_fv3_thres.sh
@@ -4,17 +4,19 @@
 res='C384'
 #res='C96'
 #base_dir=emi_C96
-base_dir=/gpfs/hps3/emc/naqfc/noscrub/Barry.Baker/emissions/emi_LIPAN_C384
+base_dir=/gpfs/dell2/emc/modeling/noscrub/Barry.Baker/emi_C384/fengsha
 
 #----------------------------------------------------
-for month in 01 02 03 04 05 06 07 08 09 10 11 12; do
-    
-    thres="0.065,0.18,0.27,0.30,0.35,0.38,0.45,0.41,0.41,0.45,0.50,0.45,9999.0"
-    #directory=/gpfs/hps3/emc/naqfc/noscrub/Barry.Baker/emissions/emi_LIPAN_C384/${month}
-    directory=${base_dir}/${month}
+#for month in 01 02 03 04 05 06 07 08 09 10 11 12; do
+for month in 03; do
+
+    echo "SETTING DEFAULT THRESHOLDS"
+    thres="0.065,0.18,0.23,0.30,0.35,0.38,0.45,0.41,0.41,0.45,0.50,0.45,9999.0"
+     directory=${base_dir}/${month}
     ./make_map.py -ut $thres -d $directory -r $res
     
-    # MODIFY AUS
+    # # MODIFY AUS
+    echo " MODIFYING AUS -------------------------------------------"
     thres="0.065,0.18,0.38,0.30,0.35,0.38,0.55,0.41,0.41,0.45,0.50,0.45,9999.0"
 
     box="108,-40,154,-11"
@@ -22,23 +24,24 @@ for month in 01 02 03 04 05 06 07 08 09 10 11 12; do
     ./modify_latlon_box.py -ut $thres -d $directory -b $box -r $res
     
     # MODIFY TAKLAMAKAN
-    
-    thres="0.065,0.18,0.23,0.30,0.35,0.38,0.45,0.41,0.41,0.45,0.50,0.45,9999.0"
+    echo " MODIFYING TALKAMAKAN -------------------------------------------"
+    thres="0.065,0.18,0.15,0.30,0.35,0.38,0.45,0.41,0.41,0.45,0.50,0.45,9999.0"
     box="77.6,36,89,41.1"
     
-    ./modify_latlon_box.py -ut $thres -d $directory -b $box -r $res
+    ./modify_latlon_box.py -ut ${thres} -d ${directory} -b $box -r $res
     
     # MODIFY CENTRAL US
-    
-    thres="0.065,0.18,0.27,0.50,0.45,0.38,0.40,0.50,0.50,0.27,0.50,0.45,9999.0"
+#    echo " MODIFYING CENTRAL US -------------------------------------------"
+    thres="0.065,0.18,0.23,0.50,0.45,0.38,0.40,0.50,0.50,0.25,0.50,0.45,9999.0"
     box="-126,25,-65,50"
-    
-    ./modify_latlon_box.py -ut $thres -d $directory -b $box -r $res
+    #box="77.6,36,89,41.1"
+    ./modify_latlon_box.py -ut $thres -d $directory -r $res --latlon_box=$box
     
     # Modify Nothern Mexico for continuity
-    thres="0.065,0.18,0.27,0.30,0.35,0.38,0.45,0.41,0.41,0.27,0.50,0.45,9999.0" 
+    echo " MODIFYING MX for Continuity -------------------------------------------"
+    thres="0.065,0.18,0.23,0.50,0.45,0.38,0.45,0.50,0.41,0.23,0.50,0.45,9999.0" 
     box="-117.95,18,-94.6,36"
     
-    ./modify_latlon_box.py -ut $thres -d $directory -b $box -r $res
+    ./modify_latlon_box.py -ut $thres -d $directory -r $res --latlon_box=$box
         
 done
